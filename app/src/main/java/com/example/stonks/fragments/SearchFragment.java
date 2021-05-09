0739a0e31f2e00dd9ebcc903ca26aa545e8be04f
Moritz Meyer <moritzmeyer@Moritzs-MacBook-Pro.fritz.box>
2021-05-09 14:13:38 +0200

    Added Python function test

--- app/src/main/java/com/example/stonks/fragments/SearchFragment.java
+++ app/src/main/java/com/example/stonks/fragments/SearchFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
 import com.example.stonks.R;
 
 public class SearchFragment extends Fragment {
@@ -32,6 +34,9 @@ public class SearchFragment extends Fragment {
                 String symbol = searchStockText.getText().toString();
                 System.out.println(symbol);
                 //Test if Python script is working
+                Python python = Python.getInstance();
+                PyObject pythonfile = python.getModule("script");
+                String answer = pythonfile.callAttr("helloworld", symbol).toString();
             }
         });
 
---
+++ app/src/main/python/script.py
@@ -0,0 +1,2 @@
+def helloworld(symbol):
+    return symbol
