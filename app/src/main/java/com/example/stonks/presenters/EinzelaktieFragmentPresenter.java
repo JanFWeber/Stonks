212ca7ac3bd4973f7df37354d5fd7650c8c4552d
floriandietsche <florian.dietsche@icloud.com>
2021-05-15 17:38:44 +0200

    rough frame for fragment_einzelaktie.xml v2

---
+++ app/src/main/java/com/example/stonks/presenters/EinzelaktieFragmentPresenter.java
@@ -0,0 +1,22 @@
+package com.example.stonks.presenters;
+
+import com.example.stonks.models.Item;
+import com.example.stonks.repository.EXCloudCalls;
+import com.example.stonks.views.viewInterfaces.IEinzelaktieFragment;
+
+public class EinzelaktieFragmentPresenter {
+
+    private IEinzelaktieFragment einzelaktieFragment;
+
+    public EinzelaktieFragmentPresenter(IEinzelaktieFragment einzelaktieFragment) {
+        this.einzelaktieFragment = einzelaktieFragment;
+    }
+
+    public void getDetailedData (){
+        Item i = new Item(); //item info ziehen
+        EXCloudCalls exCloudCalls = EXCloudCalls.getInstance();
+
+        i.
+
+    }
+}
--- app/src/main/java/com/example/stonks/views/fragments/EinzelaktieFragment.java
+++ app/src/main/java/com/example/stonks/views/fragments/EinzelaktieFragment.java
@@ -1,61 +1,29 @@
 package com.example.stonks.views.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.stonks.R;
+import com.example.stonks.StockService;
+import com.example.stonks.presenters.EinzelaktieFragmentPresenter;
+import com.example.stonks.views.viewInterfaces.IEinzelaktieFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EinzelaktieFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class EinzelaktieFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class EinzelaktieFragment extends Fragment implements IEinzelaktieFragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    StockService stockService;
+
+    private EinzelaktieFragmentPresenter einzelaktieFragmentPresenter;
 
     public EinzelaktieFragment() {
-        // Required empty public constructor
+        einzelaktieFragmentPresenter = new EinzelaktieFragmentPresenter(this);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EinzelaktieFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EinzelaktieFragment newInstance(String param1, String param2) {
-        EinzelaktieFragment fragment = new EinzelaktieFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
