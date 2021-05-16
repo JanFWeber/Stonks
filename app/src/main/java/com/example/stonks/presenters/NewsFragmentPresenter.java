f4b51184c3e47aa79adeae1531ed36d3d739a740
Jannik <jannikjn11@gmail.com>
2021-05-16 23:20:04 +0200

    News Layout/fragment

--- app/src/main/java/com/example/stonks/presenters/NewsFragmentPresenter.java
+++ app/src/main/java/com/example/stonks/presenters/NewsFragmentPresenter.java
@@ -1,10 +1,83 @@
 package com.example.stonks.presenters;
 
-public class NewsFragmentPresenter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.stonks.R;
+import com.example.stonks.views.fragments.NewsFragment;
+
+public class NewsFragmentPresenter extends RecyclerView.Adapter<NewsFragmentPresenter.ViewHolderKlasse2> {
+
+    public class ViewHolderKlasse2 extends RecyclerView.ViewHolder {
+        TextView textViewNews;
+        ImageView imageViewNews;
+
+
+        public ViewHolderKlasse2(@NonNull @org.jetbrains.annotations.NotNull View newsView) {
+            super(newsView);
+            textViewNews = (TextView) newsView.findViewById(R.id.textViewNews);
+            imageViewNews = (ImageView) newsView.findViewById(R.id.imageViewNews);
+
+        }
+
+    }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public ViewHolderKlasse2 onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+
+        View newsView1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_layout2, null);
+
+        return new ViewHolderKlasse2(newsView1);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewHolderKlasse2 holder2, int position) {
+
+        holder2.textViewNews.setText(NewsFragment.newsNachricht.get(position));
+        holder2.imageViewNews.setImageResource(NewsFragment.newsLogo.get(position));
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+
+        return NewsFragment.newsNachricht.size();
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
 
     /*
     muss von api neue Nachrichten bekommen und diese dann auf das "NewsFragment" setzten
         => Forgefertigte Kästen in denen die Nachrichten angezeigt werden mit löschoption beim wischen
     Ggf. Vermittlung zum Speicher wenn wir nicht nur aktuelle, sondern auch vergangene Nachrichten speichern und anzeigen wollen
      */
-}
+
--- app/src/main/java/com/example/stonks/views/fragments/NewsFragment.java
+++ app/src/main/java/com/example/stonks/views/fragments/NewsFragment.java
@@ -8,14 +8,54 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.stonks.R;
+import com.example.stonks.presenters.NewsFragmentPresenter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NewsFragment extends Fragment {
+
+    RecyclerView.LayoutManager rvlayoutManagerNews;
+    RecyclerView recyclerViewNews;
+    RecyclerView.Adapter rvadapterNews;
+
+    public static ArrayList<String> newsNachricht;
+    public static ArrayList<Integer> newsLogo;
+
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View n = inflater.inflate(R.layout.fragment_news, container, false);
+
+
+        newsNachricht = new ArrayList<>();
+        newsLogo = new ArrayList<>();
+
+
+
+        newsNachricht.addAll(Arrays.asList(" 3 irre günstige Divididendenaktien", " Sicherheitslücke bei neuen Apple AirTags entedeckt", "Nachricht 3", "Nachricht 4", "Nachricht 5","Nachricht 6", "Nachricht 7", "Nachricht 8", "Nachricht 9", "Nachricht 10", "Nachricht 11","Nachricht 12"));
+        newsLogo.addAll(Arrays.asList(R.drawable.ic_adidas_logo, R.drawable.ic_apple_logo_black, R.drawable.ic_biontech_logo, R.drawable.ic_coca_cola_logo, R.drawable.ic_coinbase, R.drawable.ic_johnson_and_johnson_logo, R.drawable.ic_mastercard_2019_logo, R.drawable.ic_mcdonald_s_logo, R.drawable.ic_microsoft_logo, R.drawable.ic_sap_logo, R.drawable.ic_square__inc__logo, R.drawable.ic_tesla_motors));
+
+
+        recyclerViewNews = (RecyclerView) n.findViewById(R.id.recyclerView2);
+        rvlayoutManagerNews = new LinearLayoutManager(getContext());
+        recyclerViewNews.setLayoutManager(rvlayoutManagerNews);
+
+        rvadapterNews =new NewsFragmentPresenter();
+        recyclerViewNews.setAdapter(rvadapterNews);
+
+
+
+
+
+        return n;
     }
 }
